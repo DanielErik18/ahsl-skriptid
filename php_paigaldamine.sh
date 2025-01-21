0935132654f66196e126b111e0c55a884deb9cce
Daniel <daniel-erik.kiuru@voco.ee>
2025-01-21 11:04:32 +0200

    PHP paigaldamine, korrektsete kommentaaridega

--- php_paigaldamine.sh
+++ php_paigaldamine.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Kontrollime kas apache2 teenus on installitud
+# Kontrollime kas PHP on installitud
 if dpkg -l | grep -qw php; then
-#Kui on ütleme kasutajale, et on installitud, paneme käima ja näitame statust.
+#Kui on ütleme kasutajale, et on installitud, näitame kausta ja versiooni.
         echo "PHP on juba installitud."
         which php
 	php -v
 else
-#Kui ei ole installitud paigaldame apache2 teenuse, paneme käima ja näitame statust.
+#Kui ei ole installitud paigaldame PHP, näiteme kausta ja versiooni.
         echo "Paigaldame PHP ja vajalikud lisad"
         apt install php php-mysql libapache2-mod-php
         echo "PHP on paigaldatud"
